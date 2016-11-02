661fb0c5ce1362e450e096dd0c29eed16a6c6c65
yaniv kalfa <netameta1@yahoo.com>
2016-11-02 13:31:08 -0400

    changed version and alot other stuff

--- src/index.js
+++ src/index.js
@@ -4,26 +4,11 @@ module.exports = function () {
   cookiesSize = 1024;
   defaults = {
     expires :  60*60*24/* 30 days*/,
+    domain: undefined,
+    httponly: undefined,
+    secure:undefined,
     path : '/'
   };
-
-  function prepareToSet(value){
-    try {
-      return JSON.stringify(value);
-    } catch(e) {
-      console.log(e);
-      return value;
-    }
-  }
-
-  function prepareToGet(value){
-    try {
-      return JSON.parse(value);
-    } catch(e) {
-      console.log(e);
-      return value;
-    }
-  }
 
   function parseCookies(){
     var cookiesRow, cookies;
@@ -32,7 +17,7 @@ module.exports = function () {
     cookiesRow.forEach(function(cookie){
       cookie = cookie.trim();
       cookie = cookie.split('=');
-      cookies[cookie[0]] = prepareToGet(cookie[1]);
+      cookies[cookie[0]] = cookie[1];
       cookies.__cookiesCount++;
     });
     return cookies;
@@ -40,36 +25,81 @@ module.exports = function () {
 
   function getCookie(name) {
     var cookies = parseCookies();
-    return cookies[name] || cookies
+    return name ? cookies[name] || false : cookies;
+  }
+
+  function buildCookie(content, expires, path, domain, httponly, secure) {
+
+    var cookie = [];
+    if (content){
+      cookie.push(content);
+    }
+
+    if (expires){
+      cookie.push(expires);
+    }
+
+    if (path){
+      cookie.push(path);
+    }
+
+    if (domain){
+      cookie.push(domain);
+    }
+
+    if (httponly){
+      cookie.push(httponly);
+    }
+
+    if (secure){
+      cookie.push(secure);
+    }
+
+    return cookie.join('; ') + ';';
+  }
+
+  function getCookieExpires(opts){
+    var d, expires = defaults.expires;
+    d = new Date();
+    if (opts && typeof opts.expires === 'number' && opts.expires >= 0) {
+      expires = opts.expires;
+    }
+
+    d.setTime(d.getTime()+(expires*1000));
+    return d.toUTCString();
   }
 
   function updateCookie(name, value, opts){
-    if ( !name || !value ) throw new Error('Cookie name or value is missing');
-    var d, expires, path, cookies, tempValue;
-    tempValue = prepareToSet(value);
+    if ( !name || !value ) {
+      throw new Error('Cookie name or value is missing');
+    }
+    var content, expires, path, domain, httponly, secure, cookies;
     cookies = parseCookies();
     if ( cookies.__cookiesCount >= cookiesLimit ) console.warn('You have: ' + cookies.__cookiesCount + ' Certain browsers support up to: ' + cookiesLimit);
-    if ( tempValue.length >= cookiesSize ) console.warn('Value length is: ' + tempValue.length + ' Certain browsers support up to: ' + cookiesSize);
+    if ( value.length >= cookiesSize ) console.warn('Value length is: ' + value.length + ' Certain browsers support up to: ' + cookiesSize);
 
-    d = new Date();
-    expires = (opts && typeof opts.expires === 'number' && opts.expires >= 0)
-      ? opts.expires
-      : defaults.expires;
-    d.setTime(d.getTime()+(expires*1000));
-    expires = "expires="+d.toGMTString();
-    path = opts && opts.path || defaults.path;
-    document.cookie = name + '=' + prepareToSet(value) + '; ' + expires + '; path=' + path + ';'
+    content = name + '=' + value;
+    expires = "expires=" + getCookieExpires(opts);
+    path = 'path=' + ( opts && opts.path || defaults.path );
+    domain = opts && typeof opts.domain !== 'undefined' ? 'domain=' + opts.domain : defaults.domain;
+    httponly = opts && typeof opts.httponly !== 'undefined' ? 'httponly=' + opts.httponly : defaults.httponly;
+    secure = opts && typeof opts.secure !== 'undefined' ? 'secure=' + opts.secure  : defaults.secure;
+
+    document.cookie = buildCookie(content, expires, path, domain, httponly, secure);
   }
 
-  function removeCookie(name) {
-    if ( !name ) throw new Error('Cookie name is missing');
-    updateCookie(name, true, { expires: 0 })
+  function removeCookie(name, opts) {
+    if ( !name ) {
+      throw new Error('Cookie name is missing');
+    }
+    opts = opts || {};
+    updateCookie(name, true, Object.assign({}, opts, { expires: 0 }) )
   }
 
-  return {
+  module.exports = {
     get: getCookie,
     set: updateCookie,
     update: updateCookie,
     remove: removeCookie
-  };
+  }
 };
